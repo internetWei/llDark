356d4b1e85ed6e03e1be17daa931b93e4f8a95aa
xianglong.wei <internetwei@foxmail.com>
2020-12-04 15:55:09 +0800

    fix(all): 解决无法导入

--- Classes/LLDark.h
+++ Classes/LLDark.h
@@ -8,6 +8,18 @@
 #ifndef LLDark_h
 #define LLDark_h
 
+#if __has_include(<LLDark/LLDark.h>)
+
+#import <LLDark/LLDarkManager.h>
+#import <LLDark/UIImageView+Dark.h>
+#import <LLDark/NSObject+Dark.h>
+#import <LLDark/UIColor+Dark.h>
+#import <LLDark/UIImage+Dark.h>
+#import <LLDark/UIView+Dark.h>
+#import <LLDark/LLDarkSource.h>
+
+#else
+
 #import "LLDarkManager.h"
 #import "UIImageView+Dark.h"
 #import "NSObject+Dark.h"
@@ -15,5 +27,7 @@
 #import "UIImage+Dark.h"
 #import "UIView+Dark.h"
 #import "LLDarkSource.h"
+
+#endif
 
 #endif /* LLDark_h */
